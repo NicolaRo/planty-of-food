186d34f228583f30769ec8a285c5461872d63ffd
Nico <n.rossi.studio@gmail.com>
2026-02-09 16:16:19 +0100

    -getUser.test.js -> created testunit for getUser

--- tests/controllers/userController/getUser.test.js
+++ tests/controllers/userController/getUser.test.js
@@ -15,15 +15,90 @@ const {expect} = require('chai');
 const sinon = require('sinon');
 
 //Importo userController contenente le funzioni che voglio testare
+const userController = require('../../../src/config/controllers/userController');
 
-//Descrivo cosa voglio testare: La funzione getUser all'interno del file userController
+//Importo il modello User reale 
+const User = require('../../../src/config/models/User');
+
+/* CONSOLE LOG PER DEBUG
+
+console.log(User);            // chi è User?
+console.log(userController);  // chi è getUsers? */
+
+//Descrivo il gruppo di test relativo al controller
 describe('UserController',() => {
-    describe('getUser', () => {
+
+    //Descrivo il gruppo di test relativo alla funzione getUsers
+    describe('getUsers', () => {
+
+        // afterEach dopo ogni test, ripulisce tutti gli stub e spy
+        afterEach (() => {
+            sinon.restore();
+        });
+
+        // --- SCENARIO A: SUCCESSO ---
 
         //Nel blocco "it" specifico l'aspettativa per la funzione che vado a testare
-        it('should display the details of an User created and stored in the DB', async ()=> {
+        it('should return 200 and an array of users when DB call succeeds', async () => {
+            
+            //1# ARRANGE
+            //creo un array finto di utenti (fakeUser) per simulare il DB
+            const fakeUsers = [{ id: 1, name: 'Mario', email:'mario@mail.com' }];
 
-        
-        })
-    })
-})
+            //stub di User.find per resituire i fakeUsers
+            const findStub = sinon.stub(User, 'find').resolves(fakeUsers);
+      
+            //Creo req e res finti
+            const req = { query: {} }; 
+            const res = {
+              status: sinon.stub().returnsThis(), // permette chain .json()
+              json: sinon.spy() // Spy per controllare cosa viene inviato
+            };
+      
+            //2# ACT
+            //Chiamo il controller con i finti req / res
+            await userController.getUsers(req, res);
+      
+            //3# ASSERT
+            //Verifico che User.find() sia stata chiamata
+            expect(findStub.calledOnce).to.be.true;
+
+            //Verifico che ritorni (200) se va tutto bene
+            expect(res.status.calledOnceWith(200)).to.be.true;
+
+            //Verifico che torni un json con l'elenco degli Users
+            expect(res.json.calledOnceWithMatch(fakeUsers)).to.be.true;
+
+            });
+            
+          // --- SCENARIO A: SUCCESSO ---
+
+          it('should return 500 when DB comunication break down', async() =>{
+            
+            //ARRANGE
+            //Stub di User.find che simula errore nel DB
+            const findStub = sinon.stub(User, 'find').rejects(new Error('DB failure'));
+
+            const req = { query: {} };
+            const res = {
+                status: sinon.stub().returnsThis(),
+                json: sinon.spy()
+            };
+
+            //ACT
+
+            await userController.getUsers(req, res);
+
+            //ASSERT
+            //controllo che findStube venga chiamata
+            expect(findStub.calledOnce).to.be.true;
+
+            //Controllo che il controller restituisca status 500
+            expect(res.status.calledOnceWith(500)).to.be.true;
+
+            //Controllo che json restituisca il messaggio di errore
+            expect(res.json.calledOnce).to.be.true;
+            expect(res.json.firstCall.args[0]).to.have.property('message','DB failure');
+          });
+        });
+    });
